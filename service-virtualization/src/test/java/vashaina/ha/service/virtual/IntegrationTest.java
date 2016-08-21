f06de10c4579ec0526a460096a43522576bd2c7f
Jason Andersen <andersen.jason@gmail.com>
2016-08-21 14:14:23 -0700

    had to make IntegrationTest abstract so Maven didn't try to run it as a test

--- service-virtualization/src/test/java/vashaina/ha/service/virtual/IntegrationTest.java
+++ service-virtualization/src/test/java/vashaina/ha/service/virtual/IntegrationTest.java
@@ -9,6 +9,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/service-virtualization-context.xml" })
-public class IntegrationTest {
+public abstract class IntegrationTest {
     //NOOP - just need the spring annotations
 }
